4d9c65007cbc2d89484f87d1459cbe4e051f58f9
morontt <morontt@gmail.com>
2014-09-08 23:25:20 +0300

    add task 002

---
+++ 000/euler002.js
@@ -0,0 +1,21 @@
+/**
+ * Completed on Mon, 8 Sep 2014, 23:22
+ */
+
+var f_a = 1,
+    f_b = 1,
+    f_c = 0,
+    f_summ = 0,
+    max = 4000000;
+
+do {
+    if (f_c % 2 == 0) {
+        f_summ += f_c;
+    }
+
+    f_c = f_a + f_b;
+    f_a = f_b;
+    f_b = f_c;
+} while (f_c < max);
+
+console.log(f_summ);
