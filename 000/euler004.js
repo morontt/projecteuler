29df97cff1a417ee22e5af84c8ce0b9d7cde77d8
morontt <morontt@gmail.com>
2014-09-09 00:14:50 +0300

    add task 004

---
+++ 000/euler004.js
@@ -0,0 +1,27 @@
+/**
+ * Completed on Tue, 9 Sep 2014, 00:08
+ */
+
+var i, j, k,
+    max = 0;
+
+function is_palindrome(t) {
+    var len = t.length;
+
+    if (len == 1 || len == 0) {
+        return true;
+    }
+
+    return (t[0] == t[len - 1]) && is_palindrome(t.substr(1, len - 2));
+}
+
+for (i = 100; i < 1000; i++) {
+    for (j = i; j < 1000; j++) {
+        k = i * j;
+        if (k > max && is_palindrome(k.toString())) {
+            max = k;
+        }
+    }
+}
+
+console.log(max);
