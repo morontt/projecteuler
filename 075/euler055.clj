6369a4f7c9c74c131a786df2e871661bc6b1ba31
morontt <morontt@gmail.com>
2015-04-06 23:09:05 +0300

    add task 055

--- 075/euler055.clj
+++ 075/euler055.clj
@@ -1,8 +1,10 @@
 ;; Lychrel numbers
 ;;
-;; Completed on
+;; Completed on Mon, 6 Apr 2015, 23:02
 
 (require '[clojure.string :as s])
+
+(def iteration_limit 50)
 
 (defn palindrom?
   [x]
@@ -12,3 +14,14 @@
 (defn reverse_summ
   [x]
   (+ x (bigint (s/reverse (str x)))))
+
+(defn lychrel?
+  [t, i]
+  (if (= i 0)
+    true
+    (let [z (reverse_summ t)]
+      (if (palindrom? z)
+        false
+        (recur z (- i 1))))))
+
+(println (count (filter #(lychrel? % iteration_limit) (range 1N 10000N))))
