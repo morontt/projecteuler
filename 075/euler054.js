e2db0b33414670928cf8a1b3f8bfb5e204695355
morontt <morontt@gmail.com>
2015-07-07 22:58:09 +0300

    add task 054 [work in progress]

--- 075/euler054.js
+++ 075/euler054.js
@@ -5,10 +5,24 @@
 (function () {
     var fs = require('fs');
 
-    var datalength = 1000;
+    var datalength = 10;
+
+    function Hand() {
+        this.c = [];
+        this.s = [];
+        this.h = [];
+        this.d = [];
+
+        var tmp;
+        for (var i = 0; i < arguments.length; i++) {
+            tmp = arguments[i].split('');
+            this[tmp[1].toLowerCase()].push(tmp[0]);
+        }
+    }
 
     function checkHand(string) {
         var cards = string.split(' ');
+        var hand1 = new Hand(cards[0], cards[1], cards[2], cards[3], cards[4]);
     }
 
     fs.readFile('./p054_poker.txt', {encoding: 'ascii'}, function (err, data) {
