a10e8f342a6712f612048e78468da4218033bb63
morontt <morontt@gmail.com>
2015-07-27 00:10:06 +0300

    add task 097

---
+++ 100/euler097.py
@@ -0,0 +1,36 @@
+# Large non-Mersenne prime
+#
+# Completed on Mon, 27 Jul 2015, 00:07
+
+def to_binary(x):
+    res = []
+    i = 0
+    while x > 0:
+        if x % 2 == 1:
+            res.append(i)
+        i += 1
+        x = (x - x % 2) / 2
+
+    return res
+
+def split_production(x, y):
+
+    return (x * y) % 10000000000
+
+def square_loop(t):
+    res = 2
+    while t > 0:
+        res = split_production(res, res)
+        t -= 1
+
+    return res
+
+z = to_binary(7830457)
+
+print z
+
+tmp = 28433
+for i in z:
+    tmp = split_production(tmp, square_loop(i))
+
+print tmp + 1
