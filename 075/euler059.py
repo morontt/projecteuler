08fa55dc64aeab009fd97c72cd3129c7aad492f7
morontt <morontt@gmail.com>
2015-07-30 01:58:56 +0300

    add task 059

---
+++ 075/euler059.py
@@ -0,0 +1,42 @@
+# completed 30.07.2015 01:56:32
+# 107359
+
+f = open('./p059_cipher.txt', 'r')
+tmp = f.readline().strip().split(',')
+f.close()
+
+data = map(lambda c: int(c), tmp)
+
+def cipher_decode(arr, key):
+    decoded = []
+    i = 0
+
+    for c in arr:
+        decoded.append(chr(c ^ ord(key[i % 3])))
+        i += 1
+
+    return decoded
+
+"""
+run:
+    python 075/euler059.py > ~/decoded.txt
+    cat ~/decoded.txt | grep -i word
+
+password: god
+
+for k1 in xrange(97, 123):
+    for k2 in xrange(97, 123):
+        for k3 in xrange(97, 123):
+            passwd = chr(k1) + chr(k2) + chr(k3)
+            z = cipher_decode(data, passwd)
+            s = ''
+            print passwd + '---' + s.join(z)
+"""
+
+sum = 0
+dec = cipher_decode(data, 'god')
+
+for c in dec:
+    sum += ord(c)
+
+print sum
