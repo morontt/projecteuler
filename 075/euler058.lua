39263e501c632ed5ac01cbac3d2204d6f2fa46a5
morontt <morontt@gmail.com>
2015-07-31 22:13:14 +0300

    add task 058

---
+++ 075/euler058.lua
@@ -0,0 +1,35 @@
+-- Spiral primes
+--
+-- Completed on Fri, 31 Jul 2015, 22:10
+
+function is_prime (x)
+  local res = true
+  local lim = math.floor(math.sqrt(x))
+  for i = 2, lim do
+    if x % i == 0 then
+      res = false
+      break
+    end
+  end
+  return res
+end
+
+prime = 0
+count = 1
+ratio = 1.0
+
+n = 3
+while ratio > 0.1 do
+  local d = n * n
+  for j = 1, 3 do
+    d = d - n + 1
+    if is_prime(d) then
+      prime = prime + 1
+    end
+  end
+  count = count + 4
+  ratio = prime / count
+  n = n + 2
+end
+
+print(n - 2)
