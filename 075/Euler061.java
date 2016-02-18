bbcdd06769a277452bd2cc30bb21547b16ae8516
morontt <morontt@gmail.com>
2016-02-19 00:31:45 +0200

    add task 075 - work in progress

--- 075/Euler061.java
+++ 075/Euler061.java
@@ -1,60 +1,51 @@
 /**
  * Cyclical figurate numbers
  */
+
+import java.util.function.Function;
+
+class Elem {
+    int i, lo, hi;
+
+    Elem(int i) {
+        this.i = i;
+
+        lo = i % 100;
+        hi = (i - lo) / 100;
+    }
+}
+
 public class Euler061 {
+    Elem[][] matrix = new Elem[6][100];
+    int[] lenghts = new int[6];
+
+    void fillRow(int idx, Function <Integer, Integer> f) {
+        int i = 0, j = 0, r;
+
+        do {
+            r = f.apply(i);
+            if (r > 999 && r < 10000) {
+                Elem e = new Elem(r);
+                if (e.lo > 9) {
+                    // System.out.println("hi: " + e.hi + " lo: " + e.lo);
+                    matrix[idx][j] = e;
+                    j++;
+                }
+            }
+            i++;
+        } while (r < 10000);
+
+        lenghts[idx] = j;
+    }
+
     public static void main(String[] args) {
-        int i = 0, r;
-        do {
-            r = i * (i + 1) / 2;
-            if (r > 999 && r < 10000) {
-                // System.out.println("Triangle: " + r + " i = " + i);
-            }
-            i++;
-        } while (r < 10000);
+        Euler061 eu = new Euler061();
 
-        i = 0;
-        do {
-            r = i * i;
-            if (r > 999 && r < 10000) {
-                // System.out.println("Square: " + r + " i = " + i);
-            }
-            i++;
-        } while (r < 10000);
-
-        i = 0;
-        do {
-            r = i * (3 * i - 1) / 2;
-            if (r > 999 && r < 10000) {
-                // System.out.println("Pentagonal: " + r + " i = " + i);
-            }
-            i++;
-        } while (r < 10000);
-
-        i = 0;
-        do {
-            r = i * (2 * i - 1);
-            if (r > 999 && r < 10000) {
-                // System.out.println("Hexagonal: " + r + " i = " + i);
-            }
-            i++;
-        } while (r < 10000);
-
-        i = 0;
-        do {
-            r = i * (5 * i - 3) / 2;
-            if (r > 999 && r < 10000) {
-                // System.out.println("Heptagonal: " + r + " i = " + i);
-            }
-            i++;
-        } while (r < 10000);
-
-        i = 0;
-        do {
-            r = i * (3 * i - 2);
-            if (r > 999 && r < 10000) {
-                // System.out.println("Octagonal: " + r + " i = " + i);
-            }
-            i++;
-        } while (r < 10000);
+        eu.fillRow(0, n -> n * (n + 1) / 2);
+        eu.fillRow(1, n -> n * n);
+        eu.fillRow(2, n -> n * (3 * n - 1) / 2);
+        eu.fillRow(3, n -> n * (2 * n - 1));
+        eu.fillRow(4, n -> n * (5 * n - 3) / 2);
+        eu.fillRow(5, n -> n * (3 * n - 2));
     }
 }
