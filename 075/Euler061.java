b3448f6b7300110d6bd6e81adb9858c6f0ad1f92
morontt <morontt@gmail.com>
2016-02-20 11:39:48 +0200

    add task 075 - work in progress

--- 075/Euler061.java
+++ 075/Euler061.java
@@ -18,6 +18,7 @@ class Elem {
 public class Euler061 {
     Elem[][] matrix = new Elem[6][100];
     int[] lenghts = new int[6];
+    boolean[] free = new boolean[]{true, true, true, true, true};
 
     void fillRow(int idx, Function <Integer, Integer> f) {
         int i = 0, j = 0, r;
@@ -27,7 +28,7 @@ public class Euler061 {
             if (r > 999 && r < 10000) {
                 Elem e = new Elem(r);
                 if (e.lo > 9) {
-                    // System.out.println("hi: " + e.hi + " lo: " + e.lo);
+                    //System.out.println("idx: " + idx + " i: " + e.i);
                     matrix[idx][j] = e;
                     j++;
                 }
@@ -36,6 +37,31 @@ public class Euler061 {
         } while (r < 10000);
 
         lenghts[idx] = j;
+    }
+
+    void iteration(int cnt, Elem curr, Elem first) {
+        Elem z;
+        int next = cnt + 1;
+        for (int i = 0; i < 5; i++) {
+            if (free[i]) {
+                free[i] = false;
+                for (int j = 0; j < lenghts[i]; j++) {
+                    z = matrix[i][j];
+                    if (cnt < 4) {
+                        if (z.hi == curr.lo) {
+                            System.out.print(z.i + " ");
+                            iteration(next, z, first);
+                        }
+                    } else {
+                        if (z.hi == curr.lo && z.lo == first.hi) {
+                            System.out.println(z.i + " " + first.i);
+                            System.out.println("++++++++++++++++++++++");
+                        }
+                    }
+                }
+                free[i] = true;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -47,5 +73,10 @@ public class Euler061 {
         eu.fillRow(3, n -> n * (2 * n - 1));
         eu.fillRow(4, n -> n * (5 * n - 3) / 2);
         eu.fillRow(5, n -> n * (3 * n - 2));
+
+        for (int j = 0; j < eu.lenghts[5]; j++) {
+            System.out.println("---");
+            eu.iteration(0, eu.matrix[5][j], eu.matrix[5][j]);
+        }
     }
 }
