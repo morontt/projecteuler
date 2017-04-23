0c320e612d953c1d46f2480c57acd5b8378a4d87
Alexander Harchenko <morontt@gmail.com>
2017-04-23 23:32:21 +0300

    add task 064 - work in progress, part 3

--- 075/euler064.ex
+++ 075/euler064.ex
@@ -3,22 +3,22 @@
 # Completed on
 
 defmodule Euler064 do
-  def iteration(list, x, i) when x > 0 do
-    if i < 20 do
-      r = 1.0 / x
-      ai = trunc(r)
-      xi = r - ai
-      iteration [ai | list], xi, i + 1
-    else
-      list
-    end
+  def iteration(list, x, eps) when eps < 0.1 and x > 0 do
+    r = 1.0 / x
+    ai = trunc(r)
+    xi = r - ai
+    iteration [ai | list], xi, eps / (x * x)
+  end
+
+  def iteration(list, _x, _eps) do
+    list
   end
 
   def solve() do
     x = :math.sqrt(23.0)
     a0 = trunc(x)
     x0 = x - a0
-    l = iteration [], x0, 0
+    l = iteration [], x0, 1.0e-15
     IO.inspect(l)
   end
 end
