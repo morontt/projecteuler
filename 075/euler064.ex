4cb055c8a64ae1ade3a8c6a10d54088b3c799af1
Alexander Harchenko <morontt@gmail.com>
2017-04-25 22:14:29 +0300

    task 064 - work in progress, part 4

--- 075/euler064.ex
+++ 075/euler064.ex
@@ -3,7 +3,7 @@
 # Completed on
 
 defmodule Euler064 do
-  def iteration(list, x, eps) when eps < 0.1 and x > 0 do
+  def iteration(list, x, eps) when eps < 0.01 and x > 0 do
     r = 1.0 / x
     ai = trunc(r)
     xi = r - ai
@@ -11,14 +11,23 @@ defmodule Euler064 do
   end
 
   def iteration(list, _x, _eps) do
-    list
+    Enum.reverse(list)
   end
 
-  def solve() do
-    x = :math.sqrt(23.0)
+  def check(n) do
+    x = :math.sqrt(1.0 * n)
     a0 = trunc(x)
     x0 = x - a0
     l = iteration [], x0, 1.0e-15
+    IO.inspect(n)
     IO.inspect(l)
+  end
+
+  def solve do
+    limit = 10
+    ch1 = for val <- 2..(limit * limit), do: val
+    ch2 = for val <- 1..limit, do: val * val
+    ch3 = ch1 -- ch2
+    Enum.each(ch3, &check/1)
   end
 end
