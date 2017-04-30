6cacc8f5d3ff740c346a31245fdd5843c227a576
Alexander Harchenko <morontt@gmail.com>
2017-04-30 20:12:51 +0300

    task 064

--- 075/euler064.ex
+++ 075/euler064.ex
@@ -1,33 +1,54 @@
 # Odd period square roots
 #
-# Completed on
+# Completed on Sun, 30 Apr 2017, 19:45
 
 defmodule Euler064 do
-  def iteration(list, x, eps) when eps < 0.01 and x > 0 do
-    r = 1.0 / x
-    ai = trunc(r)
-    xi = r - ai
-    iteration [ai | list], xi, eps / (x * x)
+  def iteration(_x, _s, start, a0, a1, b0, b1, n) when n > 1 and start == b0 and a0 == 1 and a1 == 1 and b1 == 1 do
+    n - 1
   end
 
-  def iteration(list, _x, _eps) do
-    Enum.reverse(list)
+  def iteration(x, s, start, a0, a1, b0, b1, n) do
+    t0 = 1.0 / (a0 * s / a1 - b0 * 1.0 / b1) |> trunc
+    a10 = a1 * a0 * b1 * b1
+    a11 = a0 * a0 * b1 * b1 * x - a1 * a1 * b0 * b0
+    b10 = t0 * a11 - a1 * a1 * b0 * b1
+    {a20, a21} = reduct(a10, a11)
+    {b20, b21} = reduct(b10, a11)
+    iteration x, s, start, a20, a21, b20, b21, n + 1
   end
 
-  def check(n) do
-    x = :math.sqrt(1.0 * n)
-    a0 = trunc(x)
-    x0 = x - a0
-    l = iteration [], x0, 1.0e-15
-    IO.inspect(n)
-    IO.inspect(l)
+  def gcd(m, 0) do
+    m
+  end
+
+  def gcd(m, n) do
+    gcd(n, rem(m, n))
+  end
+
+  def reduct(m, n) do
+    g = if m > n do
+      gcd(m, n)
+    else
+      gcd(n, m)
+    end
+
+    {div(m, g), div(n, g)}
+  end
+
+  def check(x) do
+    s = :math.sqrt(1.0 * x)
+    b = trunc(s)
+    iteration x, s, b, 1, 1, b, 1, 1
   end
 
   def solve do
-    limit = 10
+    limit = 100
     ch1 = for val <- 2..(limit * limit), do: val
     ch2 = for val <- 1..limit, do: val * val
     ch3 = ch1 -- ch2
-    Enum.each(ch3, &check/1)
+    Enum.map(ch3, &check/1)
+    |> Enum.filter(&(rem(&1, 2) == 1))
+    |> length
+    |> IO.puts
   end
 end
