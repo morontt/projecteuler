62dd5727649a4b0aa84c1dbe09ff16ead5044e57
Alexander Harchenko <morontt@gmail.com>
2019-08-11 02:39:44 +0300

    reformat code

--- 075/euler056.go
+++ 075/euler056.go
@@ -5,47 +5,47 @@
 package main
 
 import (
-    "fmt"
-    "math/big"
+	"fmt"
+	"math/big"
 )
 
 func sumDigit(z *big.Int) int64 {
-    var (
-        s int64
-    )
+	var (
+		s int64
+	)
 
-    s = 0
-    str := z.String()
+	s = 0
+	str := z.String()
 
-    for i := 0; i < len(str); i++ {
-        s += int64(str[i]) - 48
-    }
+	for i := 0; i < len(str); i++ {
+		s += int64(str[i]) - 48
+	}
 
-    return s
+	return s
 }
 
 func main() {
-    var (
-        i, j, s, max int64
-    )
+	var (
+		i, j, s, max int64
+	)
 
-    bk := new(big.Int)
-    max = 0;
+	bk := new(big.Int)
+	max = 0
 
-    for i = 2; i < 100; i++ {
-        for j = 1; j < 100; j++ {
-            bi := big.NewInt(i)
-            bj := big.NewInt(j)
+	for i = 2; i < 100; i++ {
+		for j = 1; j < 100; j++ {
+			bi := big.NewInt(i)
+			bj := big.NewInt(j)
 
-            bk.Exp(bi, bj, nil)
-            s = sumDigit(bk)
+			bk.Exp(bi, bj, nil)
+			s = sumDigit(bk)
 
-            if max < s {
-                max = s
-            }
+			if max < s {
+				max = s
+			}
 
-        }
-    }
+		}
+	}
 
-    fmt.Println(max)
+	fmt.Println(max)
 }
