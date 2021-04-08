9eada4aeb5e03e03c6ca1e87115a41ed7a27cc26
Alexander Harchenko <morontt@gmail.com>
2021-04-08 15:38:48 +0300

    task 066 - work in progress, part 3

--- 075/euler066.go
+++ 075/euler066.go
@@ -7,6 +7,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/big"
 )
 
 var (
@@ -17,6 +18,27 @@ var (
 
 type Solve struct {
 	d, x, y uint64
+}
+
+func bigSqrt(y uint64) big.Float {
+	const prec = 200
+	steps := int(math.Log2(prec))
+
+	square := new(big.Float).SetPrec(prec).SetUint64(y)
+	half := new(big.Float).SetPrec(prec).SetFloat64(0.5)
+
+	x := new(big.Float).SetPrec(prec).SetInt64(1)
+	t := new(big.Float)
+
+	for i := 0; i <= steps; i++ {
+		t.Quo(square, x)
+		t.Add(x, t)
+		x.Mul(half, t)
+	}
+
+	fmt.Printf("sqrt(%d) = %.50f\n", y, x)
+
+	return *x
 }
 
 func minimalSolution(d uint64) (Solve, bool) {
@@ -44,6 +66,8 @@ func minimalSolution(d uint64) (Solve, bool) {
 
 func otherMinimal(s Solve) (Solve, bool) {
 	i0 := s.y
+	limit := uint64(4294967295.0 / math.Sqrt(float64(s.d)))
+
 	for {
 		t0 := i0 * i0 * s.d
 		t1 := t0 + 1
@@ -60,9 +84,41 @@ func otherMinimal(s Solve) (Solve, bool) {
 		}
 
 		i0++
-		if i0 == 4294967295 {
-			return Solve{s.d, 0, 4294967295}, true
+		if i0 == limit {
+			fmt.Printf("D: %d overflow (%d)\n", s.d, limit)
+			return Solve{s.d, 0, limit}, true
 		}
+	}
+}
+
+func otherBigMinimal(s Solve) {
+	var (
+		i0, t0, t1, bigOne, bigS big.Int
+	)
+
+	i0.SetUint64(s.y)
+	bigOne.SetInt64(1)
+	bigS.SetUint64(uint64(math.Sqrt(float64(s.d))))
+
+	for {
+		t0.Mul(&i0, &i0)
+		t0.Add(&t0, &bigOne)
+
+		sqrt := new(big.Int).Set(&i0)
+		sqrt.Mul(sqrt, &bigS)
+		t1.Mul(sqrt, sqrt)
+
+		for t0.Cmp(&t1) == 1 {
+			sqrt.Add(sqrt, &bigOne)
+			t1.Mul(sqrt, sqrt)
+		}
+
+		if t0.Cmp(&t1) == 0 {
+			fmt.Printf("D: %d - %v, %v", s.d, t1, i0)
+			return
+		}
+
+		i0.Add(&i0, &bigOne)
 	}
 }
 
@@ -74,10 +130,9 @@ func main() {
 		//arr []Solve
 	)
 
-	const dmax = 1000
+	const dmax = 200
 
-	maxidx = 50000000
-	//maxidx = 1000000
+	maxidx = 1000000
 	maxsquire = maxidx * maxidx
 	squires = make(map[uint64]bool)
 	for i = 1; i <= maxidx; i++ {
@@ -96,9 +151,17 @@ func main() {
 		}
 	}
 
-	//fmt.Printf("%v\n", arr)
+	arr1 := make([]Solve, 0, 200)
+
 	for _, el := range arr {
-		n, _ := otherMinimal(el)
-		fmt.Printf("%v\n", n)
+		s, overflow = otherMinimal(el)
+		if overflow {
+			//fmt.Printf("%v\n", s)
+			arr1 = append(arr1, s)
+		}
+	}
+
+	for _, el := range arr1 {
+		otherBigMinimal(el)
 	}
 }
